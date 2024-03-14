e2e03ef71b62c732452ca0afb1e04d7e261ce9fa
ThanhHM2 <thanhhm2@vng.com.vn>
2024-03-14 18:24:29 +0700

    add pycharm config

--- mydiffusion_zeggs/end2end.py
+++ mydiffusion_zeggs/end2end.py
@@ -1,6 +1,5 @@
 import pdb
 import logging
-logging.getLogger().setLevel(logging.INFO)
 from torch.utils.data import DataLoader
 from data_loader.lmdb_data_loader import TrinityDataset
 import torch
@@ -10,14 +9,17 @@ from easydict import EasyDict
 from configs.parse_args import parse_args
 import os
 import sys
+
 [sys.path.append(i) for i in ['.', '..', '../model', '../train']]
 from utils.model_util import create_gaussian_diffusion
 from training_loop import TrainLoop
 from model.mdm import MDM
 
+logging.getLogger().setLevel(logging.INFO)
+
 
 def create_model_and_diffusion(args):
-    model = MDM(modeltype='', njoints=1141, nfeats=1, cond_mode = 'cross_local_attention3_style1', action_emb = 'tensor', audio_feat=args.audio_feat,
+    model = MDM(modeltype='', njoints=1141, nfeats=1, cond_mode='cross_local_attention3_style1', action_emb='tensor', audio_feat=args.audio_feat,
                 arch='trans_enc', latent_dim=256, n_seed=8, cond_mask_prob=0.1)
     diffusion = create_gaussian_diffusion()
     return model, diffusion
@@ -33,9 +35,9 @@ def main(args, device):
                               shuffle=True, drop_last=True, num_workers=args.loader_workers, pin_memory=True)
 
     val_dataset = TrinityDataset(args.val_data_path,
-                                       n_poses=args.n_poses,
-                                       subdivision_stride=args.subdivision_stride,
-                                       pose_resampling_fps=args.motion_resampling_framerate, model='WavLM', device=device)
+                                 n_poses=args.n_poses,
+                                 subdivision_stride=args.subdivision_stride,
+                                 pose_resampling_fps=args.motion_resampling_framerate, model='WavLM', device=device)
     test_loader = DataLoader(dataset=val_dataset, batch_size=args.batch_size,
                              shuffle=False, drop_last=True, num_workers=args.loader_workers, pin_memory=False)
 
