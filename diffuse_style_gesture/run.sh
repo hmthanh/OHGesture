5c8d1f3344b1e5205523bb9059f408738f5307cc
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-12-28 16:52:17 +0700

    update

---
+++ diffuse_style_gesture/run.sh
@@ -0,0 +1,16 @@
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./077_Neutral_2_x_1_0.wav
+
+
+
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/001_Neutral_0_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/003_Neutral_2_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/006_Sad_0_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/008_Sad_2_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/011_Happy_0_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/013_Happy_2_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/016_Relaxed_0_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/018_Relaxed_2_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/021_Old_0_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/023_Old_2_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/026_Angry_0_x_1_0.wav
+python sample.py --config=./configs/DiffuseStyleGesture.yml --gpu mps --model_path=./model000450000.pt --speech_path=./input/028_Angry_2_x_1_0.wav
